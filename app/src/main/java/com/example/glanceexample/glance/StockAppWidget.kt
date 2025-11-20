9e9985f45f04bc0eed4a92757b984fad2c920572
reira <danilova1267@icloud.com>
2025-11-20 21:20:34 +0700

    Добавление clickable-функции для элементов

--- app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
+++ app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
@@ -14,6 +14,7 @@ import androidx.glance.GlanceTheme
 import androidx.glance.Image
 import androidx.glance.ImageProvider
 import androidx.glance.LocalSize
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.SizeMode
 import androidx.glance.appwidget.cornerRadius
@@ -113,6 +114,7 @@ class StockAppWidget : GlanceAppWidget() {
     private fun Small(stateCount: Float) {
         Column(
             modifier = GlanceModifier
+                .clickable { refreshPrice() }
                 .fillMaxSize()
                 .background(GlanceTheme.colors.background)
                 .padding(8.dp)
@@ -124,6 +126,7 @@ class StockAppWidget : GlanceAppWidget() {
     private fun Medium(stateCount: Float) {
         Column(horizontalAlignment = Alignment.Horizontal.CenterHorizontally,
             modifier = GlanceModifier
+                .clickable { refreshPrice() }
                 .fillMaxSize()
                 .cornerRadius(15.dp)
                 .background(GlanceTheme.colors.background)
@@ -139,6 +142,10 @@ class StockAppWidget : GlanceAppWidget() {
             )
         }
     }
+    private fun refreshPrice() {
+        PriceDataRepo.update()
+    }
+
 }
 
 
